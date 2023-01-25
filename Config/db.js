be0c75bcb28d34d6a80d8c0fcbf048bd2240f72c
Sahnawaz Hussain <sahnawazhussain852@gmail.com>
2023-01-25 22:04:22 +0530

    url updated

--- Config/db.js
+++ Config/db.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-const URL2 = "mongodb://127.0.0.1:27017/mock10";
+// const URL2 = "mongodb://127.0.0.1:27017/mock10";
 const URL = process.env.MONGO_URL; //
-console.log("mongo url: ", URL);
+console.log("mongourl: ", URL);
 mongoose.set("strictQuery", true);
 //const connection = mongoose.connect("mongodb://127.0.0.1:27017/mock10");
 
-const connection = mongoose.connect(URL2);
+const connection = mongoose.connect(URL);
 
 module.exports = connection;
